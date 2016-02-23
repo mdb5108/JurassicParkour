c040c3708c5c48e2bdfdef6579823fff1864ed33
YashB <yash.h.bangera@gmail.com>
2016-02-22 17:23:35 -0700

    Added Number feature to place obstacles

--- Assets/Scripts/OperatorManager.cs
+++ Assets/Scripts/OperatorManager.cs
@@ -27,13 +27,12 @@ public class OperatorManager : MonoBehaviour {
 	{
         checkNumberKey();
 
-		if(Input.GetMouseButtonUp(0))
+		
+		if(placingObstacle)
 		{
-			if(placingObstacle)
-			{
-				setObstacle();
-			}
+			setObstacle();
 		}
+		
 
 		if(Input.GetMouseButtonUp(0))
 		{
@@ -56,10 +55,14 @@ public class OperatorManager : MonoBehaviour {
 
     void checkNumberKey()
     {
-        if (Input.anyKeyDown)
+        if (Input.GetKeyDown(KeyCode.Alpha1) ||
+            Input.GetKeyDown(KeyCode.Alpha2) ||
+            Input.GetKeyDown(KeyCode.Alpha3) ||
+            Input.GetKeyDown(KeyCode.Alpha4) ||
+            Input.GetKeyDown(KeyCode.Alpha5) )
         {
             string keyPreseed = Input.inputString;
-            int keyNum = int.Parse(keyPreseed);
+            int keyNum = System.Convert.ToInt32(keyPreseed);
             // KEYNUM NO OF CARDS IN HAND
             if (keyNum >= 1 && keyNum <= 5)
             {
